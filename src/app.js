4682fa28f4d6b7e876c24fff7f7c19efc1a99bc0
Aaron Gibson <aarongibson86@gmail.com>
2019-11-04 17:51:12 -0600

    removed dummy expenses

--- src/app.js
+++ src/app.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(
-  addExpense({ description: 'Rent', amount: 109500, createdAt: -100 })
-);
-store.dispatch(
-  addExpense({ description: 'Gas bill', amount: 4000, createdAt: 1000 })
-);
 
 const jsx = (
   <Provider store={store}>
