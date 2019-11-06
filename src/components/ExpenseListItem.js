53821332efc97dd730f520c16e6719afe3f1f529
Aaron Gibson <aarongibson86@gmail.com>
2019-11-06 06:50:10 -0600

    added numeral dependency for formatting numbers

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({
   description,
@@ -14,7 +15,8 @@ export const ExpenseListItem = ({
       <h3>{description}</h3>
     </Link>
     <p>
-      ${(amount / 100).toFixed(2)} - {moment(createdAt).format('MMM Do, YYYY')}
+      {numeral(amount / 100).format('$0,0.00')} -{' '}
+      {moment(createdAt).format('MMMM Do, YYYY')}
     </p>
     <p>Note: {note}</p>
   </div>
